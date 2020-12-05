1dc4e53b04b9051aa667eec0bb45bd680af9c5cc
Mrthunder25 <75518531+Mrthunder25@users.noreply.github.com>
2020-12-05 15:25:02 +0800

    Update and rename Raflyrecode.py to Mr.Thunder.py

--- Mr.Thunder.py
+++ Mr.Thunder.py
@@ -55,15 +55,15 @@ logo = """\033[1;93m█████████
 \033[1;93m█████████      \033[1;92m«----------✧----------»
 \033[1;93m ██ ██
 \033[1;93m╔════════════════════════════════════════════╗
-\033[1;93m║\033[1;96m* \033[1;93mAuthor  \033[1;93m : \033[1;93mBrother•|Mr.RaflyPakei\033[1;93m          ║
-\033[1;93m║\033[1;96m* \033[1;93mYouTube  \033[1;93m : \033[1;93m\033[4mRafly pake i\033[0m \033[1;93m║
-\033[1;93m║\033[1;96m* \033[1;93mWhatsApp \033[1;93m: \033[1;93m082_290_232_340\033[1;93m                 ║
+\033[1;93m║\033[1;96m* \033[1;93mAuthor  \033[1;93m : \033[1;93mMr.Thunder\033[1;93m          ║
+\033[1;93m║\033[1;96m* \033[1;93mWhatsApp  \033[1;93m : \033[1;93m\033[4m085_299_118_681\033[0m \033[1;93m║
+\033[1;93m║\033[1;96m* \033[1;93m \033[1;93m: \033[1;93m\033[1;93m                 ║
 \033[1;93m╚════════════════════════════════════════════╝"""
 
 def tik():
 	titik = ['.   ','..  ','... ']
 	for o in titik:
-		print("\r\033[1;96m[●] \x1b[1;93mSedang masuk by rafly pake i \x1b[1;97m"+o),;sys.stdout.flush();time.sleep(1)
+		print("\r\033[1;96m[●] \x1b[1;93mSedang masuk by Mr.Thunder \x1b[1;97m"+o),;sys.stdout.flush();time.sleep(1)
 
 
 back = 0
@@ -77,13 +77,13 @@ vuln = "\033[32mVuln"
 
 os.system("clear")
 print "\033[1;96m ============================================================="
-print  """\033[1;96m [¤] \x1b[1;93mASSALAMUALAIKUM\x1b[1;96m  \033[1;96m   [¤] \x1b[1;93mWHATSAPP : 082290232340\x1b[1;96m  
-\033[1;96m [¤] \x1b[1;93mSELAMAT DATAMG\x1b[1;96m      [¤] \x1b[1;93mLOGIN SC: UNTUK LOGIN +USERNAME+:RAFLY +PASSWORD+:PAKEI\x1b[1;96m  
-\033[1;96m [¤] \x1b[1;93mTOOLS MUHAMMAD RAFLI\x1b[1;96m  [¤] \x1b[1;93mYOUTUBE  : Rafly pake i\x1b[1;96m"""
+print  """\033[1;96m [¤] \x1b[1;93mASSALAMUALAIKUM\x1b[1;96m  \033[1;96m   [¤] \x1b[1;93mWHATSAPP : 085299118681\x1b[1;96m  
+\033[1;96m [¤] \x1b[1;93mSELAMAT DATAMG\x1b[1;96m      [¤] \x1b[1;93mLOGIN SC: UNTUK LOGIN +USERNAME+:Thunder +PASSWORD+: Gaming\x1b[1;96m  
+\033[1;96m [¤] \x1b[1;93mTOOLS Mr.Thunder\x1b[1;96m  [¤] \x1b[1;93m\x1b[1;96m"""
 print " \x1b[1;93m============================================================="
 
-CorrectUsername = "RAFLY"
-CorrectPassword = "PAKEI"
+CorrectUsername = "Thunder"
+CorrectPassword = "Gaming"
 
 loop = 'true'
 while (loop == 'true'):
